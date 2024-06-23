38624ec7625871c3b5630bc845fc7c978019aac8
Vidisha Pandey <vidisha09pandey@gmail.com>
2024-06-23 22:51:44 +0530

    api in progress

--- searchTableAPI/searchTableAPI.js
+++ searchTableAPI/searchTableAPI.js
@@ -1,54 +1,26 @@
-const data = [
-  {
-    name: "abc",
-    age: 12,
-    city: "kanpur",
-    email: "vidisha09pandey@gmail.com",
-  },
-  {
-    name: "def",
-    age: 12,
-    city: "kanpur",
-    email: "vidisha09pandey@gmail.com",
-  },
-  {
-    name: "fea",
-    age: 12,
-    city: "kanpur",
-    email: "vidisha09pandey@gmail.com",
-  },
-  {
-    name: "cdf",
-    age: 12,
-    city: "kanpur",
-    email: "vidisha09pandey@gmail.com",
-  },
-  {
-    name: "rae",
-    age: 12,
-    city: "kanpur",
-    email: "vidisha09pandey@gmail.com",
-  },
-];
+async function getData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
 
-document.getElementById("searchTab").addEventListener("keyup", function () {
-  let searchVal = event.target.value;
-  let tabData = searchTab(searchVal, data);
-  createTable(tabData);
-});
+  document.getElementById("searchTab").addEventListener("keyup", function () {
+    let searchVal = event.target.value;
+    console.log(searchVal);
+    let tabData = searchTab(searchVal, data);
+    createTable(tabData);
+  });
 
-createTable(data);
-
+  createTable(data);
+}
 function createTable(data) {
   let table = `<table>
     <tr>
-    <th>name</th>
-    <th>age</th>
-    <th>city</th><th>email</th>
+    <th>Name</th>
+  <th>email</th>
+  <th>phone</th>
     </tr>`;
 
   for (let i = 0; i < data.length; i++) {
-    table += `<tr><td>${data[i].name}</td><td>${data[i].age}</td><td>${data[i].city}</td><td>${data[i].email}</td></tr>`;
+    table += `<tr><td>${data[i].name}</td><td>${data[i].email}</td><td>${data[i].phone}</td></tr>`;
   }
   table += `</table>`;
 
@@ -60,6 +32,7 @@ function searchTab(value, arr) {
   let filteredData = [];
   value = value.toLowerCase();
   for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i].name, value);
     if (arr[i].name.includes(value)) {
       filteredData.push(arr[i]);
     }
@@ -67,3 +40,4 @@ function searchTab(value, arr) {
 
   return filteredData;
 }
+getData();
